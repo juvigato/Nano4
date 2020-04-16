4c815d33269d1f5b5a7f9f2e46ba610a58cab271
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-04-16 13:51:10 -0300

    adicionando logica de animacao

--- Inanotilidade/Inanotilidade/ViewController.swift
+++ Inanotilidade/Inanotilidade/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgBiscoito: UIImageView!
+    
+    var biscoitoImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        biscoitoImages = fillArray(total: 21, prefixo: "biscoito")
     }
-
+    
+    func fillArray(total:Int, prefixo: String) -> [UIImage] {
+        var imageArray: [UIImage] = []
+        
+        for i in 1..<total {
+            let nomeImg = "\(prefixo)\(i).pdf"
+            guard let imagem = UIImage(named: nomeImg) else { return biscoitoImages }
+            
+            imageArray.append(imagem)
+        }
+        return imageArray
+    }
+    
+    func createAnimation(imageView: UIImageView, images: [UIImage]) {
+        imageView.animationImages = images
+        imageView.animationDuration = 2.0
+        imageView.animationRepeatCount = 1
+        imageView.startAnimating()
+    }
+    
+    @IBAction func tapButton(_ sender: Any) {
+        createAnimation(imageView: imgBiscoito, images: biscoitoImages)
+        imgBiscoito.image = UIImage(named: "biscoito21")
+    }
+    
 
 }
 
